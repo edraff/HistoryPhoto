19cb41b4df47b6f7a90960ccbc0b0506298210d4
Bruno <edraff@hotmail.com>
2015-10-25 15:06:23 -0300

    Projeto
    Swipe: almost Done
    Erro Conection: Done
    ScrollView: Done
    Erro Json: Done

--- app/src/main/java/com/bjcabral/historyphoto/ListaPhotoFragment.java
+++ app/src/main/java/com/bjcabral/historyphoto/ListaPhotoFragment.java
@@ -1,16 +1,23 @@
 package com.bjcabral.historyphoto;
 
 import android.app.DownloadManager;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.bjcabral.historyphoto.model.History;
 import com.bjcabral.historyphoto.model.HistoryPhoto;
@@ -25,19 +32,50 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 /**
- * Created by Bruno on 11/10/2015.
+ * Created by Bruno on 11/10/2015 com controle de versão.
  */
-public class ListaPhotoFragment extends ListFragment {
+public class ListaPhotoFragment extends ListFragment implements SwipeRefreshLayout.OnRefreshListener {
     List<Photos> listPhoto;
+    ArrayAdapter<Photos> listAdapterPhotos;
+    SwipeRefreshLayout mSwipe;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.lista_photo_fragment,null);
+        //pegando as propriedades do novo lista_photo_fragment
+        mSwipe = (SwipeRefreshLayout) view.findViewById(R.id.swipe);
+        mSwipe.setOnRefreshListener(this);
+        return view;
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        new PhotoTask().execute();
+        listPhoto = new ArrayList<>();
+        listAdapterPhotos = new ArrayAdapter<Photos>(
+                getActivity(),android.R.layout.simple_list_item_1,listPhoto);
+        setListAdapter(listAdapterPhotos);
+        carregarPhoto();
+
     }
 
+    //Metodo onde carrega a lista de photos e verifica a conexão com a internet.
+    //
+    private  void carregarPhoto(){
+    ConnectivityManager cM = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo networkInfo = cM.getActiveNetworkInfo();
+
+    if(networkInfo!=null && networkInfo.isConnected()){
+        new PhotoTask().execute();
+    }else{
+        mSwipe.setRefreshing(false);
+        Toast.makeText(getContext(),R.string.sem_conexao,Toast.LENGTH_LONG).show();
+    }
+}
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
@@ -45,6 +83,12 @@ public class ListaPhotoFragment extends ListFragment {
         if(getActivity() instanceof AoClicarNaFoto){
             ((AoClicarNaFoto)getActivity()).clicarNaFoto(photo);
         }
+    }
+
+    @Override
+    public void onRefresh() {
+        carregarPhoto();
+
     }
 
     interface AoClicarNaFoto{
@@ -57,7 +101,6 @@ public class ListaPhotoFragment extends ListFragment {
         protected History doInBackground(Void... params) {
             OkHttpClient client = new OkHttpClient();
 
-
                 Request request = new Request.Builder()
                         .url("https://dl.dropboxusercontent.com/s/q6mhg99wevbwci9/HistoryPhoto.json?dl=0")
                         .build();
@@ -69,7 +112,7 @@ public class ListaPhotoFragment extends ListFragment {
                 Gson gson = new Gson();
                 History history = gson.fromJson(s, History.class);
                 return history;
-            } catch (IOException e) {
+            } catch (Throwable e) {
                 e.printStackTrace();
             }
             return null;
@@ -80,40 +123,17 @@ public class ListaPhotoFragment extends ListFragment {
         protected void onPostExecute(History history) {
             super.onPostExecute(history);
             if(history!=null && history.historyPhoto!=null){
-                listPhoto = new ArrayList<>();
+                listPhoto.clear();
                 for (HistoryPhoto historyPhoto:history.historyPhoto) {
                     listPhoto.addAll(historyPhoto.photos);
-                    
                 }
-                ArrayAdapter<Photos> listAdapterPhotos = new ArrayAdapter<Photos>(
-                        getActivity(),android.R.layout.simple_list_item_1,listPhoto);
-                setListAdapter(listAdapterPhotos);
+                listAdapterPhotos.notifyDataSetChanged();
+            }else{
+
+                Toast.makeText(getActivity(),R.string.erro_geral,Toast.LENGTH_LONG).show();
             }
+            mSwipe.setRefreshing(false);
         }
     }
 
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
-    }
 }
--- app/src/main/java/com/bjcabral/historyphoto/PhotoDetailActivity.java
+++ app/src/main/java/com/bjcabral/historyphoto/PhotoDetailActivity.java
@@ -20,7 +20,5 @@ public class PhotoDetailActivity extends AppCompatActivity {
                     .commit();
         }
 
-
-
     }
 }
